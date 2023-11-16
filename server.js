e51ed7d21961947f2e10c096beadb7840f055ba2
Jamil Kabir <jamilkabir719@gmail.com>
2023-11-16 11:38:58 -0500

    changed to a url strin

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 // Database pool configuration
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL
 });
+
 
 // Express app initialization
 const app = express();
