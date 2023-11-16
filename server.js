6093bae64447be0b6bb60100e0bcc59812ac109e
Jamil Kabir <jamilkabir719@gmail.com>
2023-11-16 11:45:13 -0500

    removed url string

--- server.js
+++ server.js
@@ -5,18 +5,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 // Database pool configuration
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT,
-// });
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
-
 
 // Express app initialization
 const app = express();
